dff717eb1a3a57581426a4f307a4bafa8d37b076
chixuehui <chixuehui@hotmail.com>
2025-10-04 16:24:14 +0800

    #69 数据库调整

--- src/main/ts/http/basic-info-service-api.ts
+++ src/main/ts/http/basic-info-service-api.ts
@@ -1,94 +1,94 @@
 export const API_BASIC_INFO = {
-  saveBasicInfo: {
-    url: "/rest-v1/user/basic-info/save-basic-info",
-    method: "post",
-  },
-  saveTag: {
-    url: "/rest-v1/user/self-info/save-tag",
-    method: "post",
-  },
-  userIdToUserMainInfo: {
-    url: "/seen/rest-v1/user/info/user-id-to-user-main-info",
-    method: "post",
-  },
-  userIdToRecommendUserId: {
-    url: "/rest-v1/zone/tag/user-id-to-recommend-user-id",
-    method: "post",
-  },
-  userIdToPrimaryPhotoId: {
-    url: "/seen/rest-v1/user/article/user-id-to-primary-photo-id",
-    method: "post",
-  },
-  userIdToAuthInfo: {
-    url: "/rest-v1/user/info/user-id-to-auth-info",
-    method: "post",
-  },
-  userIdToTagName: {
-    url: "/seen/rest-v1/user/info/user-id-to-tag-name",
-    method: "post",
-  },
-  userIdToPersonIntroduce: {
-    url: "/seen/rest-v1/user/info/user-id-to-person-introduce",
-    method: "post",
-  },
-  textIdToContent: {
-    url: "/seen/rest-v1/chat/history/text-id-to-content",
-    method: "post",
-  },
-  destUserIdToThumb: {
-    url: "/seen/rest-v1/user/info/dest-user-id-to-thumb",
-    method: "post",
-  },
-  destUserIdToFocus: {
-    url: "/seen/rest-v1/user/info/dest-user-id-to-focus",
-    method: "post",
-  },
-  userIdToUserIntroduceInfo: {
-    url: "/seen/rest-v1/user/info/user-id-to-user-introduce-info",
-    method: "post",
-  },
-  selfUserInfo: {
-    url: "/seen/rest-v1/user/info/self-user-info",
-    method: "get",
-  },
-  introduceTypeIndexToPersonIntroduce: {
-    url: "/rest-v1/user/info/introduce-type-index-to-person-introduce",
-    method: "post",
-  },
-  savePersonIntroduce: {
-    url: "/rest-v1/user/info/save-person-introduce",
-    method: "post",
-  },
-  savePrimaryPhoto: {
-    url: "/seen/rest-v1/user/self-info/save-primary-photo",
-    method: "post",
-  },
-  saveBasicInformationAll: {
-    url: "/rest-v1/user/self-info/save-basic-information-all",
-    method: "post",
-  },
-  selfUserIdToContactInformation: {
-    url: "/rest-v1/user/self-info/self-user-id-to-contact-information",
-    method: "post",
-  },
-  selfUserIdToEducationAndWork: {
-    url: "/rest-v1/user/self-info/self-user-id-to-education-and-work",
-    method: "post",
-  },
-  selfUserIdToBasicInformation: {
-    url: "/rest-v1/user/self-info/self-user-id-to-basic-information",
-    method: "post",
-  },
-  selfUserIdToMainPhoto: {
-    url: "/seen/rest-v1/user/self-info/self-user-id-to-main-photo",
-    method: "post",
-  },
-  selfUserIdToMainPagePhoto: {
-    url: "/seen/rest-v1/user/self-info/self-user-id-to-main-page-photo",
-    method: "post",
-  },
-  selfUserIdToTagId: {
-    url: "/rest-v1/user/self-info/self-user-id-to-tag-id",
-    method: "post",
-  },
+    saveBasicInfo: {
+        url: "/seen/rest-v1/user/basic-info/save-basic-info",
+        method: "post",
+    },
+    saveTag: {
+        url: "/rest-v1/user/self-info/save-tag",
+        method: "post",
+    },
+    userIdToUserMainInfo: {
+        url: "/seen/rest-v1/user/info/user-id-to-user-main-info",
+        method: "post",
+    },
+    userIdToRecommendUserId: {
+        url: "/rest-v1/zone/tag/user-id-to-recommend-user-id",
+        method: "post",
+    },
+    userIdToPrimaryPhotoId: {
+        url: "/seen/rest-v1/user/article/user-id-to-primary-photo-id",
+        method: "post",
+    },
+    userIdToAuthInfo: {
+        url: "/rest-v1/user/info/user-id-to-auth-info",
+        method: "post",
+    },
+    userIdToTagName: {
+        url: "/seen/rest-v1/user/info/user-id-to-tag-name",
+        method: "post",
+    },
+    userIdToPersonIntroduce: {
+        url: "/seen/rest-v1/user/info/user-id-to-person-introduce",
+        method: "post",
+    },
+    textIdToContent: {
+        url: "/seen/rest-v1/chat/history/text-id-to-content",
+        method: "post",
+    },
+    destUserIdToThumb: {
+        url: "/seen/rest-v1/user/info/dest-user-id-to-thumb",
+        method: "post",
+    },
+    destUserIdToFocus: {
+        url: "/seen/rest-v1/user/info/dest-user-id-to-focus",
+        method: "post",
+    },
+    userIdToUserIntroduceInfo: {
+        url: "/seen/rest-v1/user/info/user-id-to-user-introduce-info",
+        method: "post",
+    },
+    selfUserInfo: {
+        url: "/seen/rest-v1/user/info/self-user-info",
+        method: "get",
+    },
+    introduceTypeIndexToPersonIntroduce: {
+        url: "/rest-v1/user/info/introduce-type-index-to-person-introduce",
+        method: "post",
+    },
+    savePersonIntroduce: {
+        url: "/rest-v1/user/info/save-person-introduce",
+        method: "post",
+    },
+    savePrimaryPhoto: {
+        url: "/seen/rest-v1/user/self-info/save-primary-photo",
+        method: "post",
+    },
+    saveBasicInformationAll: {
+        url: "/rest-v1/user/self-info/save-basic-information-all",
+        method: "post",
+    },
+    selfUserIdToContactInformation: {
+        url: "/rest-v1/user/self-info/self-user-id-to-contact-information",
+        method: "post",
+    },
+    selfUserIdToEducationAndWork: {
+        url: "/rest-v1/user/self-info/self-user-id-to-education-and-work",
+        method: "post",
+    },
+    selfUserIdToBasicInformation: {
+        url: "/rest-v1/user/self-info/self-user-id-to-basic-information",
+        method: "post",
+    },
+    selfUserIdToMainPhoto: {
+        url: "/seen/rest-v1/user/self-info/self-user-id-to-main-photo",
+        method: "post",
+    },
+    selfUserIdToMainPagePhoto: {
+        url: "/seen/rest-v1/user/self-info/self-user-id-to-main-page-photo",
+        method: "post",
+    },
+    selfUserIdToTagId: {
+        url: "/rest-v1/user/self-info/self-user-id-to-tag-id",
+        method: "post",
+    },
 };
--- src/main/ts/model/consumer/user-info/BasicInfo.ts
+++ src/main/ts/model/consumer/user-info/BasicInfo.ts
@@ -1,92 +1,92 @@
-import type { EducationKey} from "../school/Education.ts";
-import type { Sex } from "./Sex.ts";
+import type {EducationValue} from "../school/Education.ts";
+import type {Sex} from "./Sex.ts";
 
 export interface BasicInfo {
-  /**
-   * 用户ID
-   */
-  userId?: number;
+    /**
+     * 用户ID
+     */
+    userId?: number;
 
-  /**
-   * 性别
-   */
-  sex?: typeof Sex.valueType;
+    /**
+     * 性别
+     */
+    sex?: typeof Sex.valueType;
 
-  /**
-   * 学历
-   */
-  education?: EducationKey;
+    /**
+     * 学历
+     */
+    education?: EducationValue;
 
-  /**
-   * 出生年份
-   */
-  birthYear?: number;
+    /**
+     * 出生年份
+     */
+    birthYear?: number;
 
-  /**
-   * 已毕业
-   */
-  graduated?: boolean;
+    /**
+     * 已毕业
+     */
+    graduated?: boolean;
 }
 
 /**
  * 用户介绍的资料
  */
 export interface UserIntroduceInfo {
-  /**
-   * 用户ID
-   */
-  userId: number;
-  /**
-   * 用户认证
-   */
-  userAuthId: number;
-  /**
-   * 昵称
-   */
-  aliasName: string;
-  /**
-   * 现居城市名
-   */
-  currentResidenceCityName: string;
-  /**
-   * 出生省份名
-   */
-  birthPlaceProvinceName: string;
-  /**
-   * 出生城市名
-   */
-  birthPlaceCityName: string;
-  /**
-   * 出生年份
-   */
-  birthYear: number;
-  /**
-   * 星座
-   */
-  constellation: string;
-  /**
-   * 体重（千克）
-   */
-  weightKg: number;
+    /**
+     * 用户ID
+     */
+    userId: number;
+    /**
+     * 用户认证
+     */
+    userAuthId: number;
+    /**
+     * 昵称
+     */
+    aliasName: string;
+    /**
+     * 现居城市名
+     */
+    currentResidenceCityName: string;
+    /**
+     * 出生省份名
+     */
+    birthPlaceProvinceName: string;
+    /**
+     * 出生城市名
+     */
+    birthPlaceCityName: string;
+    /**
+     * 出生年份
+     */
+    birthYear: number;
+    /**
+     * 星座
+     */
+    constellation: string;
+    /**
+     * 体重（千克）
+     */
+    weightKg: number;
 
-  /**
-   * 身高（厘米）
-   */
-  statureCm: number;
-  /**
-   * 学历
-   */
-  educationId: number;
-  /**
-   * 就读学校
-   */
-  schoolName: string;
-  /**
-   * 职业
-   */
-  workPositionName?: string;
-  /**
-   * 工作公司
-   */
-  workCompanyName: string;
+    /**
+     * 身高（厘米）
+     */
+    statureCm: number;
+    /**
+     * 学历
+     */
+    educationId: number;
+    /**
+     * 就读学校
+     */
+    schoolName: string;
+    /**
+     * 职业
+     */
+    workPositionName?: string;
+    /**
+     * 工作公司
+     */
+    workCompanyName: string;
 }
