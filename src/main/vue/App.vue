1e9cf4f3d65cf6bcf6d08a27508b636316ff26ef
chixuehui <chixuehui@hotmail.com>
2025-12-30 23:57:20 +0800

    #78 依赖升级

--- src/main/vue/App.vue
+++ src/main/vue/App.vue
@@ -1,7 +1,14 @@
 <script lang="ts" setup>
+import {NavBar as VanNavBar} from "vant";
+import {NumberKeyboard as VanNumberKeyboard} from "vant";
 
 document.title = "转觅";
 </script>
 <template>
-  <router-view />
+  <router-view/>
+  <!-- 开启顶部安全区适配 -->
+  <van-nav-bar safe-area-inset-top/>
+
+  <!-- 开启底部安全区适配 -->
+  <van-number-keyboard safe-area-inset-bottom/>
 </template>
